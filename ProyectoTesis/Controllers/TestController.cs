e3e8559ff8beac3430c3c7ecd5d8dbfbaae25e3b
JorgeRiveraMio <jorgeriveramio1008@gmail.com>
2025-09-21 15:52:54 -0500

    JRM:Se creo pagina de inicio y cokkies

---
+++ ProyectoTesis/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoTesis.Controllers
+{
+    public class TestController : Controller
+    {
+      
+        public ActionResult ComenzarTest()
+        {
+          
+            var sessionId=Guid.NewGuid().ToString();
+            HttpContext.Session.SetString("SessionId", sessionId);
+
+            //Colocar paginas a que va hacer dirigida
+            return View();  
+        }
+
+
+    
+    }
+}
--- ProyectoTesis/Program.cs
+++ ProyectoTesis/Program.cs
@@ -29,8 +29,24 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 #endregion
 
+
+#region Sesion
+// Agregar servicios de sesión
+builder.Services.AddDistributedMemoryCache(); // almacén en memoria
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = ".VocacionalApp.Session"; // nombre de la cookie
+    options.IdleTimeout = TimeSpan.FromHours(1);    // cuánto dura la sesión
+    options.Cookie.HttpOnly = true;                 // solo accesible desde servidor
+    options.Cookie.IsEssential = true;              // no bloqueado por GDPR
+});
+
+
+#endregion
+
 var app = builder.Build();
 
+app.UseSession(); // <- muy importante, habilita el middleware
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
