db6463332a77faf0a51b34aaa5a179be8be381e7
Marco Jimenez <marcofabianj@hotmail.com>
2025-10-08 17:54:23 -0500

    Recreate clean InitialCreate migration for Render

--- ProyectoTesis/Migrations/20251008225330_InitialCreate.Designer.cs
+++ ProyectoTesis/Migrations/20251008225330_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using ProyectoTesis.Data;
 namespace ProyectoTesis.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20251008224711_InitialCreate")]
+    [Migration("20251008225330_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
