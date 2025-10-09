2d2b9e46fb7a31bfa0e50afe4a93faaca37cc7f2
Marco Jimenez <marcofabianj@hotmail.com>
2025-10-09 18:22:56 -0500

    se corrigio servicio de email

--- ProyectoTesis/Services/EmailService.cs
+++ ProyectoTesis/Services/EmailService.cs
@@ -11,35 +11,50 @@ namespace ProyectoTesis.Services
         private readonly string _apiKey = Environment.GetEnvironmentVariable("MAILJET_API_KEY");
         private readonly string _secretKey = Environment.GetEnvironmentVariable("MAILJET_SECRET_KEY");
 
-
         public async Task EnviarCorreoConPdfAsync(string destinatario, string asunto, string cuerpoHtml, byte[] pdfBytes)
         {
             var client = new MailjetClient(_apiKey, _secretKey);
+
             var request = new MailjetRequest
             {
                 Resource = Send.Resource,
             }
-            .Property(Send.FromEmail, "marcofabianjz@gmail.com")
-            .Property(Send.FromName, "Vocacional App")
-            .Property(Send.Subject, asunto)
-            .Property(Send.HtmlPart, cuerpoHtml)
-            .Property(Send.Recipients, new JArray {
-                new JObject { { "Email", destinatario } }
-            });
-
-            if (pdfBytes != null && pdfBytes.Length > 0)
+            .Property(Send.Messages, new JArray
             {
-                request.Property(Send.Attachments, new JArray {
-                    new JObject {
-                        { "ContentType", "application/pdf" },
-                        { "Filename", "ResultadosVocacionales.pdf" },
-                        { "Base64Content", Convert.ToBase64String(pdfBytes) }
-                    }
-                });
-            }
+                new JObject
+                {
+                    ["From"] = new JObject
+                    {
+                        ["Email"] = "marcofabianjz@gmail.com",
+                        ["Name"] = "Vocacional App"
+                    },
+                    ["To"] = new JArray
+                    {
+                        new JObject
+                        {
+                            ["Email"] = destinatario,
+                            ["Name"] = "Usuario"
+                        }
+                    },
+                    ["Subject"] = asunto,
+                    ["HTMLPart"] = cuerpoHtml,
+                    ["Attachments"] = pdfBytes != null && pdfBytes.Length > 0
+                        ? new JArray
+                        {
+                            new JObject
+                            {
+                                ["ContentType"] = "application/pdf",
+                                ["Filename"] = "ResultadosVocacionales.pdf",
+                                ["Base64Content"] = Convert.ToBase64String(pdfBytes)
+                            }
+                        }
+                        : null
+                }
+            });
 
             var response = await client.PostAsync(request);
             Console.WriteLine($"Mailjet response: {response.StatusCode}");
+            Console.WriteLine($"Mailjet body: {response.GetData()}");
         }
     }
 }
